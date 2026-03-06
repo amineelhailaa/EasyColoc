2e4e5e384966119fa6d9aa95e786d43ec32d058e
AMINE <amineelhailaa@gmail.com>
2026-03-06 04:21:24 +0000

    Heroku: trust proxy headers for HTTPS scheme

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'membership.role' => MembershipRole::class,
             'admin' => AdminMiddleware::class,
